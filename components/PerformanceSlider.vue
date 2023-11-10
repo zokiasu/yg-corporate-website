70c43e70c203b6434b2cd58193fc66a69e23dbf3
Pierrick Taïly <pierrick.tly@gmail.com>
2023-11-11 00:04:02 +0100

    Refactor video player component styles

--- components/PerformanceSlider.vue
+++ components/PerformanceSlider.vue
@@ -5,16 +5,16 @@
       :centeredSlides="true"
       @swiper="onSwiper"
       @slideChange="onSlideChange"
-      class="aspect-video max-h-[20vh] min-h-[20vh] rounded sm:max-h-[30vh] sm:min-h-[30vh] md:max-h-[40vh] md:min-h-[40vh] lg:max-h-[50vh] lg:min-h-[50vh] 2xl:max-h-[60vh] 2xl:min-h-[60vh]"
+      class="aspect-video max-h-[30vh] min-h-[30vh] rounded md:max-h-[40vh] md:min-h-[40vh] lg:max-h-[50vh] lg:min-h-[50vh] 2xl:max-h-[60vh] 2xl:min-h-[60vh]"
     >
       <SwiperSlide v-for="i in idVideos" :key="i">
         <div class="flex items-center justify-center">
           <iframe
-            :src="`https://www.youtube.com/embed/${i}?disablekb=1&controls=0&rel=0&iv_load_policy=3&cc_load_policy=0&playsinline=1&showinfo=0&modestbranding=1&fs=0&loop=1&playlist=${i}&vq=hd1080`"
+            :src="`https://www.youtube.com/embed/${i}?disablekb=1&rel=0&allowfullscreen=1&iv_load_policy=3&cc_load_policy=0&playsinline=1&showinfo=0&modestbranding=1&fs=0&loop=1&playlist=${i}&vq=hd1080`"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen="true"
-            class="aspect-video max-h-[20vh] min-h-[20vh] sm:max-h-[30vh] sm:min-h-[30vh] md:max-h-[40vh] md:min-h-[40vh] lg:max-h-[50vh] lg:min-h-[50vh] 2xl:max-h-[60vh] 2xl:min-h-[60vh]"
+            class="aspect-video max-h-[30vh] min-h-[30vh] rounded md:max-h-[40vh] md:min-h-[40vh] lg:max-h-[50vh] lg:min-h-[50vh] 2xl:max-h-[60vh] 2xl:min-h-[60vh]"
           />
         </div>
       </SwiperSlide>
--- components/Section/PerformanceVideo.vue
+++ components/Section/PerformanceVideo.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="bg-ygdistinctblack flex h-screen items-center justify-center">
-    <div class="flex flex-col overflow-hidden p-5">
-      <p class="staatliches w-fit pb-10 text-5xl font-bold text-white 2xl:text-7xl">
+  <div class="flex h-screen items-center justify-center bg-ygdistinctblack">
+    <div class="flex flex-col space-y-10 overflow-hidden">
+      <p
+        class="staatliches ml-5 w-fit text-5xl font-bold text-white lg:ml-0 2xl:text-7xl"
+      >
         VIBE THE FLOW.
       </p>
       <LazyPerformanceSlider class="xl:mx-40" />
-      <CustomButton class="text-ygdistinctblack flex items-center gap-2">
+      <CustomButton
+        class="mr-5 flex items-center gap-2 self-end text-ygdistinctblack lg:mr-0"
+      >
         <IconPlus class="h-5 w-5" />
         <p>More YG Artists's MV</p>
       </CustomButton>
